89590dcfdb2d96224955ae14ff64d8bdad33a82b
Kishan_Gupta <guptakishan428@gmail.com>
2024-10-08 15:58:44 +0530

    fixes title

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
@@ -7,6 +12,7 @@ import Signup from "./pages/Signup";
 const routes = (
   <Router>
     <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/dashboard" exact element={<Home />} />
       <Route path="/login" exact element={<Login />} />
       <Route path="/signup" exact element={<Signup />} />
